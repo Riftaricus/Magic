1f95c4fd3a1842de3be3d3de94c8de00232bc341
Riftaricus <morris@dasookwat.com>
2026-08-27 12:02:55 +0200

    Updated small stuff

--- main.py
+++ main.py
@@ -16,16 +16,26 @@ def get_data(api_url: str, path: str):
 
     r = requests.get(api_url + path, headers=headers)
     r.status_code
+
+    if r.status_code != 200:
+        return None
+
     return r.json()
 
 def transfer_to_csv(json_data: dict):
-    df = pd.read_json("output/data.json")
-    df.to_csv("/home/morris/Documents/Projects/Active/Magic/output/data.csv", encoding="utf-8", index=False)
-
+    try:
+        df = pd.read_json("output/data.json")
+        df.to_csv("/home/morris/Documents/Projects/Active/Magic/output/data.csv", encoding="utf-8", index=False)
+    except Exception:
+        return False
+    return True
 
 if __name__ == "__main__":  
     print("Requesting data")
     data = get_data(BASE_API_URL, "/sets")
+    if (data == None):
+        print("An error occured getting the data from the API...")
+        exit
     print("Succesfully requested data")
     print("Filtering Data")
     processed_data = []
@@ -43,8 +53,8 @@ if __name__ == "__main__":
     print("Saving data to output/data.json")
     with open("output/data.json", mode="w") as file:
         file.write(json.dumps(processed_data))
-    print("Saved data  to output/data.json")
+    print("Saved data to output/data.json")
     print("Saving CSV data to output/data.csv")
-    transfer_to_csv(data)
-    print("Saved CSV data to output/data.csv")
+    worked = transfer_to_csv(data)
+    print(("Succesfully" if worked == True else "Incorrectly") + " saved CSV data to output/data.csv")
 
